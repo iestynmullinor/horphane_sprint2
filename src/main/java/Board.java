08808854a262309b40e4ac2531360d7123516f94
Aidanddow <2468915D@student.gla.ac.uk>
2024-11-01 09:59:09 +0000

    Initialized board and game methods

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,3 +1,36 @@
+import java.util.Arrays;
+
 public class Board {
-    
+
+
+    private final int size;
+    private Object[][] gameBoard;
+
+    public Board(int size){
+        this.size = size;
+        this.gameBoard   = new Object[size][size];
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public Object[][] getGameBoard(){
+        return gameBoard;
+    };
+
+    public void initializeBoard() {
+//        Create a board of size 'size' and output it
+
+    }
+
+    public void show() {
+        for (Object[] row : gameBoard) {
+            for (Object cell : row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println("\n");
+        }
+    }
+
 }
---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //System.out.println('Hello World');
+        Board b = new Board(4);
+
+        b.show();
+    }
+}
